0b0e473d8525cc217d744608ca2565120b56a7d0
Torstein <141224356+Trind03@users.noreply.github.com>
2024-06-22 01:13:36 +0200

    Update
    Added clean up destructor

--- string.cpp
+++ string.cpp
@@ -1,14 +1,20 @@
 #include <cstdint>
 #include "string.h"
 
+string::string(const char* m_other): m_data(new char[length(m_other) + 1])
+{
+
+}
+
+string::~string()
+{
+    delete[] m_data;
+    m_data = nullptr;
+}
+
 uint32_t length(const char*& m_data)
 {
     uint32_t length = 0;
     while(m_data[length] != '\0')   length++;
     return length;
-}
-
-string::string(const char* m_other): m_data(new char[length(m_other) + 1])
-{
-
 }
--- string.h
+++ string.h
@@ -4,6 +4,7 @@ class string
 {
 public:
     string(const char* m_other);
+    ~string();
     int length(const char*& m_data);
 
     char* m_data;
