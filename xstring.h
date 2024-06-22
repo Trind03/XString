d76202bc497639919273409d57eb3b67dddb3741
Torstein <141224356+Trind03@users.noreply.github.com>
2024-06-23 00:05:06 +0200

    Privatization of values

--- xstring.h
+++ xstring.h
@@ -7,7 +7,12 @@ public:
     string(const char* m_other);
     ~string();
     int length(const char* m_other);
+    friend std::ostream& operator<<(std::ostream& stream, string& str) { stream << str.m_data; return stream; }
+    friend const char* operator (string& str)
+    {
+
+    }
+private:
     char* m_data;
     int m_size;
-    friend std::ostream& operator<<(std::ostream& stream, string& str) { stream << str.m_data; return stream; }
 };
