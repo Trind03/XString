80f5aa695f7539898869212aa5150de57f4b9bf5
Torstein <141224356+Trind03@users.noreply.github.com>
2024-09-16 21:54:35 +0200

    Added test fixture

--- testing/main.cpp
+++ testing/main.cpp
@@ -2,6 +2,31 @@
 #include "../src/xstring.h"
 #include <gtest/gtest.h>
 
+class sample : public ::testing::Test
+{
+public:
+    void reset()
+    {
+        data = "Hello, World!";
+    }
+protected:
+    void SetUp() override
+    {
+        data = "Hello, World!";
+    }
+
+    void TearDown() override
+    {
+        data = "";
+    }
+    string data;
+};
+
+TEST_F(sample,concat)
+{
+    ASSERT_EQ(data,"Hello, World!");
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc,argv);
